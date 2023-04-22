5995386706e966230b5c0dd69f4116a901d068eb
bhargavsai15 <36848893+bhargavsai15@users.noreply.github.com>
2023-04-22 18:04:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 /*  your css code here. If applicable */
 const btn=document.querySelector('form>input');
 const selectedItem = document.getElementById('colorSelect');
-const selectedIndex = selectedItem.selectedIndex;
+
 
 btn.addEventListener('click', () => {
+  const selectedIndex = selectedItem.selectedIndex;
   if (selectedIndex != -1) {
     selectedItem.options[selectedIndex].remove();
   }
