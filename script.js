5d942a08f8d09319206b7a1dc70aa6160e3c6b68
bhargavsai15 <36848893+bhargavsai15@users.noreply.github.com>
2023-04-22 18:03:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
 /*  your css code here. If applicable */
 const btn=document.querySelector('form>input');
 const selectedItem = document.getElementById('colorSelect');
+const selectedIndex = selectedItem.selectedIndex;
 
 btn.addEventListener('click', () => {
-  let index = 0;
-  selectedItem.childNodes.forEach(item => {
-    if (item.value == selectedItem.value) {
-      item.style.display = 'none';
-    }
-  })
-  
-  for (let i = 0; i < selectedItem.length; i++){
-    if (selectedItem[i].style.display != 'none') {
-      selectedItem.selectedIndex = index;
-      return;
-    } else {
-      index++;
-    }
+  if (selectedIndex != -1) {
+    selectedItem.options[selectedIndex].remove();
   }
-
-  selectedItem.selectedIndex = -1;
 })
