09e651b631d3371c5326e2f02d16947b3885a9fa
bhargavsai15 <36848893+bhargavsai15@users.noreply.github.com>
2023-04-22 17:53:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+/*  your css code here. If applicable */
+const btn=document.querySelector('form>input');
+const selectedItem = document.getElementById('colorSelect');
+
+btn.addEventListener('click', () => {
+  let index = 0;
+  selectedItem.childNodes.forEach(item => {
+    if (item.value == selectedItem.value) {
+      item.style.display = 'none';
+    }
+  })
+  console.log(selectedItem.children);
+
+  for (let i = 0; i < selectedItem.length; i++){
+    if (selectedItem[i].style.display != 'none') {
+      selectedItem.selectedIndex = index;
+      return;
+    } else {
+      index++;
+    }
+  }
+
+  selectedItem.selectedIndex = -1;
+})
